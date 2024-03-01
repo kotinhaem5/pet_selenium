2942f58dd80ebb0001fad82d8c4e24879d4be1b0
Văn Đảng <50283561+kotinhaem5@users.noreply.github.com>
2024-03-01 17:15:43 +0700

    Update AbstractTest.java

--- Pet_Selenium/action/commons/AbstractTest.java
+++ Pet_Selenium/action/commons/AbstractTest.java
@@ -2,31 +2,54 @@ package commons;
 
 import java.util.concurrent.TimeUnit;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public abstract class AbstractTest {
-	private WebDriver driver;
-	private String path = System.getProperty("user.dir");
+    protected enum Brower{
+        CHROME,FIREFOX,CHROMEHEADLESS,FIREFOXHEADLESS;
+    }
+    private WebDriver driver;
+    private String path = System.getProperty("user.dir");
 
-	protected WebDriver getBrowserDriver(String browerName) {
-		switch (browerName) {
-		case "firefox":
-			System.setProperty("webdriver.gecko.driver", path + "//WebDriver//geckodriver.exe");
-			driver = new FirefoxDriver();
-			break;
-		case "chrome":
-			System.setProperty("webdriver.chrome.driver", path + "//WebDriver//chromedriver.exe");
-			driver = new ChromeDriver();
-			break;
-
-		default:
-			throw new RuntimeException("Please chose new browser");
-		}
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("https://facebook.com");
-		return driver;
-	}
+    protected WebDriver getBrowserDriver(String browerName) {
+        Brower brower =  Brower.valueOf(browerName.toUpperCase());
+        switch (brower) {
+            case FIREFOX:
+//                WebDriverManager.firefoxdriver().setup();
+                System.setProperty("webdriver.gecko.driver", path + "//WebDriver//geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+            case CHROME:
+//                WebDriverManager.chromedriver().setup();
+//                WebDriverManager.chromedriver().version("77.0.36").setup();
+                System.setProperty("webdriver.chrome.driver", path + "//WebDriver//chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+            case CHROMEHEADLESS:
+                System.setProperty("webdriver.chrome.driver", path + "//WebDriver//chromedriver.exe");
+                ChromeOptions chromeopt = new ChromeOptions();
+                chromeopt.addArguments("headless");
+                chromeopt.addArguments("window-size=1920*1080");
+                driver = new ChromeDriver(chromeopt);
+                break;
+            case FIREFOXHEADLESS:
+                System.setProperty("webdriver.chrome.driver", path + "//WebDriver//chromedriver.exe");
+                FirefoxOptions fireOpt = new FirefoxOptions();
+                fireOpt.addArguments("headless");
+                fireOpt.addArguments("window-size=1920*1080");
+                driver = new FirefoxDriver(fireOpt);
+                break;
+            default:
+                throw new RuntimeException("Please chose new browser");
+        }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://google.com");
+        return driver;
+    }
 
 }
